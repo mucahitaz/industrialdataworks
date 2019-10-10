3e11a74c46a517aecc6de63fc6e48bf71fa20841
t23631int <mucahittaha.az@tai.com.tr>
2019-10-10 13:44:13 +0300

    changed my dataset

--- mucahit.py
+++ mucahit.py
@@ -1,5 +1,12 @@
 import pandas as pd
 
-df = pd.read_csv (r'/home/administrator/PycharmProjects/Uni/drivers.csv', encoding = "ISO-8859-1")   #read the csv file (put 'r' before the path string to address any special characters, such as '\'). Don't forget to put the file name at the end of the path + ".csv"
-print (df)
+pd.set_option('display.max_columns', 25)
 
+df = pd.read_csv (r'/home/administrator/PycharmProjects/Uni/data/Rainier_Weather.csv', encoding = "ISO-8859-1")   #read the csv file (put 'r' before the path string to address any special characters, such as '\'). Don't forget to put the file name at the end of the path + ".csv"
+# print(df.head())
+
+to_drop= ['Wind Direction AVG']
+
+df.drop(to_drop , inplace = True, axis=1)
+
+print(df.head())
