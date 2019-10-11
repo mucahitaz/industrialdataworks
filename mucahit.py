9000780f8a5ce362b7504355b59313fb8dcf60a0
t23631int <mucahittaha.az@tai.com.tr>
2019-10-11 10:34:16 +0300

    made some changes

--- mucahit.py
+++ mucahit.py
@@ -1,12 +1,33 @@
 import pandas as pd
 
+pd.set_option('display.max_rows', 50)
 pd.set_option('display.max_columns', 25)
+pd.set_option('display.width', 1000)
 
-df = pd.read_csv (r'/home/administrator/PycharmProjects/Uni/data/Rainier_Weather.csv', encoding = "ISO-8859-1")   #read the csv file (put 'r' before the path string to address any special characters, such as '\'). Don't forget to put the file name at the end of the path + ".csv"
-# print(df.head())
+df = pd.read_csv (r'/home/administrator/PycharmProjects/Uni/data/Video_Games_Sales_as_at_22_Dec_2016.csv', encoding = "ISO-8859-1")
 
-to_drop= ['Wind Direction AVG']
+'''Başlangıçta veri setini incelemek ve fikir edinmek için kullanılabilecek komutlar'''
 
-df.drop(to_drop , inplace = True, axis=1)
+def StarterFunc():
+    print(df.head())  #İlk 5 satır görüntülenir.
 
-print(df.head())
+
+'''Eksik değer içeren kayıtların silinmesi yöntemi'''
+
+def DropMissingValues():
+    print("Amount of NaN values before execution\n", df.isnull().sum())
+    df.dropna(how='any' , inplace= True) #Buradaki inplace kısmı gerçek veri üzerinde etkide bulunup bulunmama kararıdır.
+    print ("Amount of NaN values after execution\n" , df.isnull().sum())
+    return df # Sutunlardaki toplam boş veri sayıları öğrenilir.
+
+
+'''Eksik değer içeren verilerin medyan yardımı ile doldurulması'''
+
+def MedianFillingFunc(self):
+    median = df[self].median()
+    df[self].fillna(median, inplace=True)
+    print("Median of {} column is".format(self) , median)
+    return df
+
+print(DropMissingValues())
+print(MedianFillingFunc())  #tbd value problem çıkarıyor.
